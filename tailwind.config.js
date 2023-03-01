2c9509acaf1fe899fb80fc79fda88116ddfbbf62
Nasib Farooq <83704259+iamnasib@users.noreply.github.com>
2023-03-01 20:38:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*/**.html"]
+  ,
+  theme: {
+    extend: {
+      colors: {
+        'btn': '#2596be',
+      },
+    },
+  },
+  plugins: [],
+}
